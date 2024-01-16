1a05e6c44e322f8e4d6bff3e3da936f9c34f630c
janymau <156783467+janymau@users.noreply.github.com>
2024-01-16 19:34:44 +0600

    Create Python Comments.py

---
+++ Python Comments.py
@@ -0,0 +1,20 @@
+#Example 1
+#This is a comment
+print("Hello, World!")
+#Example 2
+print("Hello, World!") #This is a comment
+#Example 3
+#print("Hello, World!")
+print("Cheers, Mate!")
+#Example 4
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+#Example 4
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!")
