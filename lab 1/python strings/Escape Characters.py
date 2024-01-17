db02b9d60c6cf439fc06bebbf143f709960dd6d9
janymau <156783467+janymau@users.noreply.github.com>
2024-01-17 12:36:52 +0600

    added new folder and files

---
+++ lab 1/python strings/Escape Characters.py
@@ -0,0 +1,12 @@
+#Example 1
+txt = "We are the so-called \"Vikings\" from the north."
+Code	Result
+#\'	Single Quote	
+#\\	Backslash	
+#\n	New Line	
+#\r	Carriage Return	
+#\t	Tab	
+#\b	Backspace	
+#\f	Form Feed	
+#\ooo	Octal value	
+#\xhh	Hex value	
---
+++ lab 1/python strings/String Concatenation.py
@@ -0,0 +1,10 @@
+#example 1
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+#example 2
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
